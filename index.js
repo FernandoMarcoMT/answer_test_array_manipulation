30e9aae52181f796852b5435ae4b15f0a70c061a
Fernando Marco <marco3072003@gmail.com>
2025-05-28 15:00:11 +0700

    fix data

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const prices = ["Diskon", "Sale", "Diskon", "Sale", "Sale"];
 
 // Condition 1
 const products1 = ["baju", "celana", "topi", "jaket", "sepatu"];
-const colors1 = ["merah", "kuning", "hijau", "pink", "ungu", "maroon","biru"];
+const colors1 = ["merah", "kuning", "hijau", "pink", "ungu", "maroon"];
 const prices1 = ["Diskon", "Sale", "Diskon", "Sale", "Sale"];
 
 
